41977c5c6e98725cb4c0dc984ad052244f85cadf
brandonk8325 <brandonkerpel@gmail.com>
2020-06-17 08:58:45 -0500

    Add files via upload

---
+++ src/components/Footer.svelte
@@ -0,0 +1,61 @@
+<style>
+
+@media(min-width:788px){
+    .small{ 
+        display:none !important;
+    }
+}
+@media(min-width:500px){
+    .left{
+    float:left;
+    text-align:left;
+    background-color: #1f1f1f;
+}
+
+.right{
+    float: right;
+    text-align: right;
+    background-color: #1f1f1f;
+}
+.middle{
+      display: block;
+  margin-left: auto;
+  margin-right: auto;
+    text-align:center;
+    background-color: #1f1f1f;
+}
+
+}
+@media(max-width:499px){
+    .vrysmall{
+        display:none;
+    }
+}
+.black, .container{
+    background-color: #1f1f1f !important;
+    width: 100%;
+    padding-bottom:10px;
+}
+p{
+    color:#f5f5f5;  
+}
+.container{
+    padding-top:15px;
+    font-family:Bookman, UWC;   
+}
+
+
+</style>
+<div class = "black">
+    <div class = "container">
+        <div class = "left">
+        <p>Office Address: <span class = "vrysmall"><br></span><br><strong><bold>BY APPOINTMENT ONLY</bold></strong><br>420 Lake Cook Road<br>Suite 120<br>Deerfield, IL 60015</p>
+        </div>
+        <div class = "right">
+        <p>Contact Info: <span class = "vrysmall"><br></span><span class = "vrysmall"><br></span><span class="small"><br></span>Phone: 847-374-8257<br>Fax: 847-374-8258<br>Email: david@kerpellaw.com</p>
+        </div>
+        <div class = "middle">
+        <p>Mailing Address:<span class = "vrysmall"><br></span><br>Law Offices of David S. Kerpel, LLC<br>655 Deerfield<span class = "small"><br></span> Road<br>Suite 100<br>Deerfield, IL 60015</p>
+        </div>
+    </div>
+</div>
---
+++ src/components/Nav.svelte
@@ -0,0 +1,103 @@
+<script>
+    export let segment;
+
+    const handleClick = () => alert('I warned you!');
+</script>
+
+<style>
+    .header {
+        padding: 3rem 3rem 0;
+        width: 100%;
+        
+    }
+    .header_info{
+        font-family: Bookman, URW !important;
+        text-transform: none;
+        letter-spacing: 1px;
+        padding-bottom:10px;
+    }
+    .header_text{
+        font-size:25px;
+        font-family: Bookman, URW !important;
+    }
+    .header_text::first-letter, .first_letter{
+        font-size: 35px !important;
+    }
+
+    .header h1 {
+        margin: 0;
+    }
+
+    .navbar-brand {
+        margin: 0;
+    }
+
+    .nav-item {
+        margin: 0 1rem;
+        text-align: center;
+    }
+
+    .text-light {
+        color: #f5f5f5 !important;
+    }
+
+    .navbar-dark, .big-primary, .header, .main {
+        background-color: #1f1f1f !important;
+    }
+
+    .text-medium {
+        color: #1f1f1f;
+    }
+
+    h3 {
+        font-size: 15px;
+        padding-top: 7px;
+    }
+    nav{
+        padding:5px !important;
+    }    
+    .nav-link, .navbar-brand{
+        font-family:bookman, UWC;
+    }    
+</style>
+<div class="header bg-primary d-inline-block">
+    <h1 class="text-light text-center header_text">Law <span class = "first_letter">O</span>ffices <span class = "first_letter">o</span>f <span class = "first_letter">D</span>avid <span class = "first_letter">S</span>. <span class = "first_letter">K</span>erpel, <span class = "first_letter">LLC</span></h1>
+    <h3 class="text-medium text-center text-light header_info">-Matrimonial and Family Law Representation for Clients in Northern
+        Illinois, Lake and Cook Counties-</h3>
+</div>
+<nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-primary">
+    <div class="container">
+        <p class="d-sm-block d-md-none navbar-brand">{segment||"Home"}</p>
+
+        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarColor01"
+                aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="navbar-collapse collapse" id="navbarColor01" style="">
+            <ul class="navbar-nav m-auto">
+                <li class={`nav-item ${segment===undefined && "active"}`}>
+                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class={`nav-item ${segment==="about" && "active"}`}>
+                    <a class="nav-link" href="about">About</a>
+                </li>
+                <li class={`nav-item ${segment==="articles" && "active"}`}>
+                    <a class="nav-link" href="articles">Articles</a>
+                </li>
+                <li class={`nav-item ${segment==="FAQ" && "active"}`}>
+                    <a class="nav-link" href="FAQ">FAQ</a>
+                </li>
+                <li class={`nav-item ${segment==="resources" && "active"}`}>
+                    <a class="nav-link" href="resources">Resources</a>
+                </li>
+                <li class={`nav-item ${segment==="reviews" && "active"}`}>
+                    <a class="nav-link" href="reviews">Reviews</a>
+                </li>
+                <li class={`nav-item ${segment==="contact" && "active"}`}>
+                    <a class="nav-link" href="contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
