37a83cea3399ec16b50912a73fa65930d2cc3da3
brandonk8325 <brandonkerpel@gmail.com>
2020-06-18 09:26:34 -0500

    Add files via upload

---
+++ src/routes/FAQ.svelte
@@ -0,0 +1,97 @@
+<style>
+
+.card-header{
+    background-color:#1f1f1f;
+    color:#f5f5f5;
+}
+.card-body{
+    background-color:#f5f5f5;
+    color: #1f1f1f;
+}
+.card{
+    border: 0px;
+    padding:10px;
+    background-color:#f5f5f5
+}
+.container{
+    font-family:Bookman, UWC;
+}
+.whole-thing{
+  background-color:#f5f5f5;
+  padding-bottom:1px;
+}
+</style>
+
+
+<svelte:head>
+    <title>FAQ - Kerpel Law</title>
+</svelte:head>
+<div class = "whole-thing">
+  <div class = "container">
+    <div class="card border-light mb-3">
+      <div class="card-header">How much will my divorce cost?</div>
+      <div class="card-body">
+        <p class="card-text"></p>
+      </div>
+    </div>
+    <div class="card border-light mb-3">
+      <div class="card-header">How long will my divorce take?</div>
+      <div class="card-body">
+        <p class="card-text">There is no easy answer to this question although it is probably one of the most commonly asked. The duration of a divorce will depend on many factors. The most common questions to ask which will affect the duration of your divorce are:</p>
+      <ul>
+      <li>Is custody an issue?</li>
+      <li>Is there a business to value?</li>
+      <li>Is there a lot of real estate to value?</li>
+      <li>Is dissipation an issue?</li>
+      <li>Is maintenance or child support an issue?</li>
+      <li>Is one of the spouses self-employed?</li>
+      <li>Will the parties cooperate with the discovery process?</li>
+      <li>Are the attorneys intent on litigating, or are they willing to enter into reasonable settlement discussions?</li>
+      </ul>
+      </div>
+    </div>
+    <div class="card border-light mb-3" >
+      <div class="card-header">What is mediation, and when is it appropriate?</div>
+      <div class="card-body">
+        <p class="card-text">When the parties involved are able to communicate effectively, and trust each other to disclose all relevant information, mediation is a non-confrontational solution to resolving your marital difficulties. The Law Offices of David S. Kerpel offers the option of using skilled mediators when it is appropriate to the circumstances.</p>
+      </div>
+    </div>
+    <div class="card border-light mb-3">
+      <div class="card-header">How can I control my costs?</div>
+      <div class="card-body">
+        <p class="card-text">You can control costs by assisting your attorney and professionals associated with your proceedings. Gather your pertinent information and documents. Make your own copies. Set realistic goals. Be concise, and focus on facts relevant to your case.</p>
+      </div>
+    </div>
+    <div class="card border-light mb-3">
+      <div class="card-header">When is a neutral business or real estate appraiser appropriate?</div>
+      <div class="card-body">
+        <p class="card-text">Similar to participating in mediation, a neutral business or real estate appraiser should only be chosen when the parties have been completely forthright in the exchange of information, and are willing to compromise in their pursuit of litigating their respective rights. Agreeing to use a mutual expert can save parties thousands of dollars, and months and months of extended litigation.</p>
+      </div>
+    </div>
+    <div class="card border-light mb-3">
+      <div class="card-header">How can I modify support or custody orders?</div>
+      <div class="card-body">
+        <p class="card-text"> A support or custody order can only be modified by filing a petition usually in the court where the prior order establishing the custody or support order was entered. Especially important to remember is the fact that child support can only be modified from the time a petition is filed and noticed before the court. If you believe a payer's income has substantially increased since the last support order was entered, you should file the petition as soon as possible. If you choose to wait to file, you will not be able to obtain a retroactive increase from before the time you file the petition.</p>
+      </div>
+    </div>
+    <div class="card border-light mb-3">
+      <div class="card-header">What is discovery and when do I need it?</div>
+      <div class="card-body">
+        <p class="card-text">Discovery is the process by which your attorney obtains relevant information concerning every facet of your case. Discovery is probably the most important aspect of your case. Whether the issues of your divorce or paternity deal with finances, custody, visitation, or even grounds of your divorce, discovery is the process by which you obtain all of the relevant facts to support your case. Discovery can take many forms including depositions, written requests for information (often called interrogatories), and subpoenas.</p>
+      </div>
+    </div>
+    <div class="card border-light mb-3">
+      <div class="card-header">What is dissipation?</div>
+      <div class="card-body">
+        <p class="card-text">Dissipation is strictly defined as property which is improperly used for the sole benefit of one spouse, for a purpose unrelated to the marriage, at a time when the marriage is undergoing an irretrievable breakdown. Some commons examples of dissipation include:</p>
+        <ul>
+        <li>Spending money on a boyfriend or girlfriend</li>
+        <li>Gambling losses</li>
+        <li>Reckless investing of funds</li>
+        <li>Destruction of personal property</li>
+        <li>Failing to maintain the mortgage on a home</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ src/routes/about.svelte
@@ -0,0 +1,41 @@
+<style>
+.portrait{
+    display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 25%;
+  padding:15px;
+}
+p{
+    text-align: justify;
+    font-family: Bookman, UWC;
+    font-size:15px;
+    font-weight: 100;
+    padding:10px;
+}
+h6{
+    text-align:center;
+    font-family: Bookman, UWC;
+}
+.container{
+    background-color:#f5f5f5;
+}
+</style>
+
+<svelte:head>
+    <title>About - Kerpel Law</title>
+</svelte:head>
+<div class = "container">
+<img class = "portrait" src = "david-kerpel.jpg" alt = "David Kerpel">
+<p>David Kerpel has been practicing family and matrimonial law for over sixteen years. He is experienced in handling family law matters at every stage of the legal process. David's areas of expertise include divorce, paternity, child support and modification of child support, child custody and visitation, spousal support and maintenance, prenuptial agreements, division of marital and non-marital property, and orders of protection.</p>
+<h6>Knowledgeable and Compassionate Representation</h6>
+<p>A skillful and accomplished attorney, David Kerpel is also a staunch advocate for children. He will work tirelessly to achieve solutions that are in the best interests of his client and the children involved
+<br><br>
+David has been practicing in the area of family and matrimonial law for over sixteen years. He has worked at two of the largest matrimonial law firms in the Chicago area, before opening his own practice in 2003. David attended the University of Iowa for his undergraduate degree, and obtained his Juris Doctor from the DePaul School of Law. While attending DePaul, he was able to expand his knowledge of matrimonial law by participating in an externship with a Cook County Domestic Relations Judge. When preparing cases for trial, David combines his knowledge and experience with his attention to detail. David has successfully defended against and petitioned for orders of protection, and has successfully litigated both custody and financial trials.
+<br><br>
+David is admitted to the State of Illinois Bar, and is also licensed to practice law before the U.S. District Court, Northern District of Illinois, and the United States Supreme Court.
+<br><br>
+David has written articles for the Illinois State Bar Association and the Lake County Bar Association, and has lectured for the Lake County Bar Association and The Lilac Tree.
+<br>
+</p>
+</div>
